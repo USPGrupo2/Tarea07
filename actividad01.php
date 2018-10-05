6503d8a5086f3897f062231407b8dac912a063cd
USP <USP@C303-10>
2018-10-04 22:41:19 -0500

    segundo envio Antonio

--- actividad01.php
+++ actividad01.php
@@ -6,3 +6,15 @@
 </form>
         
         //PRUEBA ANTONIO
+        <h4>Lista de numeros:</h4>
+<table border="1">
+<tr>
+ <td>1</td> 
+</tr>
+<tr>
+ <td>2</td>
+</tr>
+<tr> 
+ <td>3</td>
+</tr>        
+</table>
